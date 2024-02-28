cc40d3f8dbf99a1a4d73ce9ef90e1cef223de9c6
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 11:44:46 +0500

    Power of two

---
+++ PowerOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+	internal class PowerOfTwo
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+			int number;
+
+			number = random.Next();
+
+			int finishNumber = 1;
+			int exponent = 0;
+			int multiplier = 2;
+
+			while (finishNumber <= number)
+			{
+				finishNumber *= multiplier;
+				exponent++;
+			}
+
+			Console.WriteLine($"Число: {number}");
+			Console.WriteLine($"Минимальная степень {multiplier}, превосходящая число: {multiplier} " +
+				$"в степени {exponent} = {finishNumber}");
+		}
+	}
+}
